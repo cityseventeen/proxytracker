e7ac50536db1da55b34ddc0e05212eed055f8c21
CitySeventeen <raffaelebalsamo91@gmail.com>
2022-05-25 16:47:06 +0200

    add possibility to test with proxy returned by trap

--- test/proxy-tracker.test.js
+++ test/proxy-tracker.test.js
@@ -114,127 +114,142 @@ describe('inserimento delle callback', () => {
     });
   });
   
+  for(let test of [{title: 'trap doesnt return proxy, but real value', flag: false}]){
+    describe(`in classe with ${test.title}`, ()=>{
+      class classe{
+        static paramstatic = 8;
+        constructor(arg){this.argomenti = arg; this.constr = 5; this.obj = {};}
+        met1(){this.uno = 1; return 'called';}
+        met2(arg){this.due = arg; return 'called';}
+        static met3(){}
+        static oggetto = {}
+      }
+      const testTrap = new testTrapGenerator(classe, test.flag);
 
-  describe('in classe', ()=>{
-    class classe{
-      static paramstatic = 8;
-      constructor(arg){this.argomenti = arg; this.constr = 5; this.obj = {};}
-      met1(){this.uno = 1; return 'called';}
-      met2(arg){this.due = arg; return 'called';}
-      static met3(){}
-    }
-    const testTrap = new testTrapGenerator(classe);
-    
-    for(let trap of   ['construct', 'get', 'defineProperty', 'deleteProperty',
-                      'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
-                      'isExtensible', 'ownKeys', 'preventExtensions', 'set',
-                      'setPrototypeOf'])
-    {
-       testTrap[trap]();                             
-    }
-  });
-  describe('in classe derivata', () => {
-    class base{
-      constructor(arg){this.base = arg;}
-    }
-    class derivata extends base{
-      constructor(arg){super(arg); this.derivata = arg + 100;}
-      metodo_derivata(){this.metodo = 8; return 'qualcosa';}
-    }
-    const testTrap = new testTrapGenerator(derivata);
-    
-    for(let trap of   ['construct', 'get', 'defineProperty', 'deleteProperty',
-                      'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
-                      'isExtensible', 'ownKeys', 'preventExtensions', 'set',
-                      'setPrototypeOf'])
-    {
-       testTrap[trap]();                             
-    }
-  });
-  describe('in oggetto', ()=>{
-    const object = {param: 5, metodo(){return 8;}};
-    const testTrap = new testTrapGenerator(object);
-    
-    for(let trap of   ['get', 'defineProperty', 'deleteProperty',
-                      'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
-                      'isExtensible', 'ownKeys', 'preventExtensions', 'set',
-                      'setPrototypeOf'])
-    {
-       testTrap[trap]();                             
-    }
-  });
-  describe('in funzione', ()=>{
-    const funzione = function(){};
-    const testTrap = new testTrapGenerator(funzione);
-    
-    for(let trap of   ['construct', 'apply', 'get', 'defineProperty', 'deleteProperty',
-                      'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
-                      'isExtensible', 'ownKeys', 'preventExtensions', 'set',
-                      'setPrototypeOf'])
-    {
-       testTrap[trap]();                             
-    }
-  });
-  describe('in array', ()=>{
-    const array = [];
-    const testTrap = new testTrapGenerator(array);
-    
-    for(let trap of   ['get', 'defineProperty', 'deleteProperty',
-                      'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
-                      'isExtensible', 'ownKeys', 'preventExtensions', 'set',
-                      'setPrototypeOf'])
-    {
-       testTrap[trap]();                             
-    }
-  });
-  
+      for(let trap of   ['construct', 'get', 'defineProperty', 'deleteProperty',
+                        'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
+                        'isExtensible', 'ownKeys', 'preventExtensions', 'set',
+                        'setPrototypeOf'])
+      {
+         testTrap[trap]();                             
+      }
+    });
+    describe(`in classe derivata with ${test.title}`, () => {
+      class base{
+        constructor(arg){this.base = arg;}
+      }
+      class derivata extends base{
+        constructor(arg){super(arg); this.derivata = arg + 100;}
+        metodo_derivata(){this.metodo = 8; return 'qualcosa';}
+        static oggetto = {}
+      }
+      const testTrap = new testTrapGenerator(derivata, test.flag);
+
+      for(let trap of   ['construct', 'get', 'defineProperty', 'deleteProperty',
+                        'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
+                        'isExtensible', 'ownKeys', 'preventExtensions', 'set',
+                        'setPrototypeOf'])
+      {
+         testTrap[trap]();                             
+      }
+    });
+
+    describe(`in oggetto with ${test.title}`, ()=>{
+      const object = {param: 5, metodo(){return 8;}, oggetto: {}};
+      const testTrap = new testTrapGenerator(object, test.flag);
+
+      for(let trap of   ['get', 'defineProperty', 'deleteProperty',
+                        'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
+                        'isExtensible', 'ownKeys', 'preventExtensions', 'set',
+                        'setPrototypeOf'])
+      {
+         testTrap[trap]();                             
+      }
+    });
+    describe(`in funzione with ${test.title}`, ()=>{
+      const funzione = function(){};
+      const testTrap = new testTrapGenerator(funzione, test.flag);
+
+      for(let trap of   ['construct', 'apply', 'get', 'defineProperty', 'deleteProperty',
+                        'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
+                        'isExtensible', 'ownKeys', 'preventExtensions', 'set',
+                        'setPrototypeOf'])
+      {
+         testTrap[trap]();                             
+      }
+    });
+    describe(`in array with ${test.title}`, ()=>{
+      const array = [];
+      const testTrap = new testTrapGenerator(array, test.flag);
+
+      for(let trap of   ['get', 'defineProperty', 'deleteProperty',
+                        'getOwnPropertyDescriptor', 'getPrototypeOf', 'has',
+                        'isExtensible', 'ownKeys', 'preventExtensions', 'set',
+                        'setPrototypeOf'])
+      {
+         testTrap[trap]();                             
+      }
+    });
+  }
   function testTrapGenerator(entita, value_returned_is_proxy){
     const any_trap = 'get';
-    let handler_for_forcing_returning_proxy_from_trap;
-    if(value_returned_is_proxy){
-      handler_for_forcing_returning_proxy_from_trap = {};
-      handler_for_forcing_returning_proxy_from_trap[any_trap] = function(){};
-    }
+    const forceToReturnProxy = function(from_trap){
+      const handler = {};
+      if(value_returned_is_proxy){
+        const handler_for_forcing_returning_proxy_from_trap = {};
+        handler_for_forcing_returning_proxy_from_trap[any_trap] = function(){};
+        handler[from_trap] = handler_for_forcing_returning_proxy_from_trap;
+      }
+      return handler;
+    };
+    const doesSureReturnedIsProxy = function(value, entita){
+      console.dir(value);
+      console.dir(typeof entita);
+      assert(util.types.isProxy(value), 'error in test. The value returned must be a proxy');
+    };
     this.apply = function(){
       it('handler = {apply: callback} inserisce la callback in apply', () => {
         const handler = {apply: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        track();
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('apply'));
+        let value = track();
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.construct = function(){
       it('handler = {construct: callback} inserisce la callback in construct', () => {
         const handler = {construct: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        new track(5);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('construct'));
+        let value = new track(5);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.defineProperty = function(){
       it('handler = {defineProperty: callback} inserisce la callback in defineProperty', () => {
         const handler = {defineProperty: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('defineProperty'));
         track.new_prop = 'valore';
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
       it('handler = {defineProperty: callback} inserisce la callback in defineProperty', () => {
         const handler = {defineProperty: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.defineProperty(track, 'new_prop', {value: 8});
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('defineProperty'));
+        let value = Object.defineProperty(track, 'new_prop', {value: 8});
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.deleteProperty = function(){
       it('handler = {deleteProperty: callback} inserisce la callback in deleteProperty', () => {
         const handler = {deleteProperty: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('deleteProperty'));
         Reflect.deleteProperty(track, 'prop');
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
       it('handler = {deleteProperty: callback} inserisce la callback in deleteProperty', () => {
         const handler = {deleteProperty: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('deleteProperty'));
         delete track.parametro;
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
@@ -242,29 +257,33 @@ describe('inserimento delle callback', () => {
     this.get = function(){
       it('handler = {get: callback} inserisce la callback in get', () => {
         const handler = {get: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('get'));
         track.parametro;
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        let value = track.oggetto;
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.getOwnPropertyDescriptor = function(){
       it('handler = {getOwnPropertyDescriptor: callback} inserisce la callback in getOwnPropertyDescriptor', () => {
         const handler = {getOwnPropertyDescriptor: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.getOwnPropertyDescriptor(track, 'prop');
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('getOwnPropertyDescriptor'));
+        let value = Object.getOwnPropertyDescriptor(track, 'oggetto');
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.getPrototypeOf = function(){
       it('handler = {getPrototypeOf: callback} inserisce la callback in getPrototypeOf', () => {
         const handler = {getPrototypeOf: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.getPrototypeOf(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('getPrototypeOf'));
+        let value = Object.getPrototypeOf(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
       it('handler = {getPrototypeOf: callback} inserisce la callback in getPrototypeOf', () => {
         const handler = {getPrototypeOf: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('getPrototypeOf'));
         track instanceof Object;
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
@@ -272,13 +291,13 @@ describe('inserimento delle callback', () => {
     this.has = function(){
       it('handler = {has: callback} inserisce la callback in has', () => {
         const handler = {has: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('has'));
         'param' in track;
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
       it('handler = {has: callback} inserisce la callback in has', () => {
         const handler = {has: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('has'));
         Reflect.has(track, 'param');
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
@@ -286,7 +305,7 @@ describe('inserimento delle callback', () => {
     this.isExtensible = function(){
       it('handler = {isExtensible: callback} inserisce la callback in isExtensible', () => {
         const handler = {isExtensible: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('isExtensible'));
         Object.isExtensible(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
@@ -294,41 +313,46 @@ describe('inserimento delle callback', () => {
     this.ownKeys = function(){
       it('handler = {ownKeys: callback} inserisce la callback in ownKeys', () => {
         const handler = {ownKeys: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.keys(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('ownKeys'));
+        let value = Object.keys(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
       it('handler = {ownKeys: callback} inserisce la callback in ownKeys', () => {
         const handler = {ownKeys: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.getOwnPropertyNames(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('ownKeys'));
+        let value = Object.getOwnPropertyNames(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.preventExtensions = function(){
       it('handler = {preventExtensions: callback} inserisce la callback in preventExtensions', () => {
         const handler = {preventExtensions: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.seal(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('preventExtensions'));
+        let value = Object.seal(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
       it('handler = {preventExtensions: callback} inserisce la callback in preventExtensions', () => {
         const handler = {preventExtensions: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.freeze(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('preventExtensions'));
+        let value = Object.freeze(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
       it('handler = {preventExtensions: callback} inserisce la callback in preventExtensions', () => {
         const handler = {preventExtensions: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
-        Object.preventExtensions(track);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('preventExtensions'));
+        let value = Object.preventExtensions(track);
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
+        if(value_returned_is_proxy) doesSureReturnedIsProxy(value, entita);
       });
     };
     this.set = function(){
       it('handler = {set: callback} inserisce la callback in set', () => {
         const handler = {set: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('set'));
         track.parametro = 8;
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
@@ -336,7 +360,7 @@ describe('inserimento delle callback', () => {
     this.setPrototypeOf = function(){
       it('handler = {setPrototypeOf: callback} inserisce la callback in setPrototypeOf', () => {
         const handler = {setPrototypeOf: cbs.cb1};
-        let track = new ProxyTracker(entita, handler);
+        let track = new ProxyTracker(entita, handler, forceToReturnProxy('setPrototypeOf'));
         Reflect.setPrototypeOf(track, {});
         expect(bridge).to.be.an('array').that.include(`callback cb1 chiamata`);
       });
